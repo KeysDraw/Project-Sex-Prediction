981da18ed5b9462afbf171b94f3f4542930f548e
KeysDraw <75135014+KeysDraw@users.noreply.github.com>
2021-02-25 21:18:04 +0100

    updated readme and sp.py

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ Use multiple models, for the sake of trying out, try to find sample values that 
     -Abr. GP(C)
     -designed to solve regression and probabilistic classification problems
     -User manual: https://scikit-learn.org/stable/modules/gaussian_process.html#gaussian-process
+
+## RandomForestClassifier
+    -Abr. RF(C)
+    -Builds multiple decision trees (->Forest) and merges them together. By merging RandomForests this achieves reduced variance in the results.
+    -User manual: https://scikit-learn.org/stable/modules/ensemble.html#forest
+
+## DecisionTreeClassifier
+    -Abr. DT(C)
+    -
+    -User manual: https://scikit-learn.org/stable/modules/tree.html#tree
--- SP.py
+++ SP.py
@@ -3,15 +3,12 @@ from sklearn import ensemble
 from sklearn import gaussian_process
 from sklearn import neighbors
 
+#different learning models
 dtc = tree.DecisionTreeClassifier()
 rfc = ensemble.RandomForestClassifier()
 gpc = gaussian_process.GaussianProcessClassifier()
 knc = neighbors.KNeighborsClassifier()
 
-# CHALLENGE - create 3 more classifiers...
-# 1 
-# 2
-# 3
 
 # [height, weight, shoe_size]
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
@@ -22,7 +19,7 @@ Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
      'female', 'male', 'male']
 
 
-# CHALLENGE - ...and train them on our data
+#train them on data
 dtc = dtc.fit(X, Y)
 rfc = rfc.fit(X, Y)
 gpc = gpc.fit(X, Y)
@@ -35,7 +32,7 @@ prediction_rfc = rfc.predict([[190,70,z]])
 prediction_gpc = gpc.predict([[190, 70, z]])
 prediction_knc = knc.predict([[190,70,z]])
 
-# CHALLENGE compare their reusults and print the best one!
+#compare their reusults
 
 print(prediction_dtc)
 
